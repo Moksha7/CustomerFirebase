9158ed555533e24a69613e4bedf339b475a4e98c
Moksha <shahmoksha1999@gmail.com>
2021-07-19 13:14:42 +0530

    Project SetUp , Layout , Navigation done

---
+++ app/src/main/java/com/example/customerfirebase/ui/fragment/RegisterFragment.kt
@@ -0,0 +1,28 @@
+package com.example.customerfirebase.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.customerfirebase.databinding.FragmentRegistrationBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentRegistrationBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/customerfirebase/ui/fragment/SplashFragment.kt
@@ -0,0 +1,4 @@
+package com.example.customerfirebase.ui.fragment
+
+class SplashFragment {
+}
